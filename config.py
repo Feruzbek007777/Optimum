b4cad8932e5cebfff8e9caee010764adae6fd04b
Felix <bobir0263@gmail.com>
2025-09-24 15:32:01 +0300

    Tayyor

--- config.py
+++ config.py
@@ -1,10 +1,26 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # .env faylini yuklaydi
+# .env fayldan o‘qish
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))  # Masalan: 6587587517
-DATABASE_PATH = os.getenv("DATABASE_PATH", "data.db")
-CONTACT_INFO = os.getenv("CONTACT_INFO", "")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
+
+ADMINS = [6587587517]  # Admin ID lari
+DATABASE_PATH = "data.db"
+
+CONTACT_INFO = """
+📞 Biz bilan bog'lanish:
+
+📍 Manzil: Farg'ona : Yozyovon 
+📞 Telefon: +998 99 998 64 21
+📧 Telegram : @optimum_LA
+
+🕒 Ish vaqtimiz: 
+Dushanba - Yakshanba : 6:00 - 20:00
+
+Made by @Fellixboi 
+"""
+
+# Kanal ma’lumotlari
+CHANNEL_USERNAME = "@optimum_LA"
